dbc9fa6492534d90974480ce984263c76a514eb8
anees107 <anees107@hotmail.com>
2025-04-28 12:10:32 +0200

    kommentar

--- src/main/java/com/example/_portfolio_2/ApplicationTest.java
+++ src/main/java/com/example/_portfolio_2/ApplicationTest.java
@@ -13,68 +13,82 @@ import java.util.*;
 
 public class ApplicationTest extends Application {
 
+    // Model-objekt lavet til at hente data og ECTS-point
     private final Model model = new Model();
 
+    // Definerer ComboBox, TextArea og Label til bachelor progam
     private final ComboBox<String> comboBoxProgram = new ComboBox<>();
     private final ComboBox<String> comboBoxCourses = new ComboBox<>();
     private final TextArea textAreaCourses = new TextArea();
     private final Label labelECTSProgram = new Label("0");
 
+    // Definerer ComboBox, TextArea og label til vores første fag
     private final ComboBox<String> comboBoxSubject1 = new ComboBox<>();
     private final ComboBox<String> comboBoxSubjectCourses1 = new ComboBox<>();
     private final TextArea textAreaSubject1 = new TextArea();
     private final Label labelECTSSubject1 = new Label("0");
 
+    // Definerer ComboBox, TextArea og label til vores andet fag
     private final ComboBox<String> comboBoxSubject2 = new ComboBox<>();
     private final ComboBox<String> comboBoxSubjectCourses2 = new ComboBox<>();
     private final TextArea textAreaSubject2 = new TextArea();
     private final Label labelECTSSubject2 = new Label("0");
 
+    // Definerer ComboBox, TextArea og label til vores valgfag
     private final ComboBox<String> comboBoxElectives = new ComboBox<>();
     private final TextArea textAreaElectives = new TextArea();
     private final Label labelECTSElectives = new Label("0");
 
+    // Label til at vise det totale ECTS-point
     private final Label labelECTSTotal = new Label("Total ECTS: 0");
 
+    // Label til de forskellige sektioner, og deres overskrifter
     private final Label label1 = new Label("Bachelor Program");
     private final Label label2 = new Label("Subject Module 1");
     private final Label label3 = new Label("Subject Module 2");
     private final Label label4 = new Label("Electives");
 
-    /*// Labels til ECTS
-    Label labelECTSProgram = new Label("0");
-    Label labelECTSSubject1 = new Label("0");
-    Label labelECTSSubject2 = new Label("0");
-    Label labelECTSElectives = new Label("0");
-    Label labelECTSTotal = new Label("total 0");
-
-    Model model = new Model();*/
-
     @Override
+    // Start-metoden kaldes automatisk når JavaFX-progammet startes
     public void start(Stage stage) {
+        // Her oprettes et GridPane-layout til at lave nogle GUI-komponenter.
         GridPane root = new GridPane();
+
+        // Her initialiserer opsæting for de forskellige sektioner som vi opretter.
         setupProgamSelection();
         setupSubject1Selection();
         setupSubject2Selection();
         setupElectivesSelection();
         setupLayout(root);
 
+       // Opretter scenen med root, layoutet og her bestemmes størrelsen.
         Scene scene = new Scene(root, 800, 400);
+
+        // Vinduets titel
         stage.setTitle("Bachelor Program");
+
+        // Tilføjer scenen til vores vindue, og viser det
         stage.setScene(scene);
         stage.show();
     }
+
+    // Laver bachelor progam sektion
     private void setupProgamSelection() {
+        // Tilføjer programmer fra modellen til comboBox program
         comboBoxProgram.getItems().addAll(model.baseProgram());
 
+        // Når brugeren vælger et program
         comboBoxProgram.setOnAction(event -> {
             String selectedProgram = comboBoxProgram.getValue();
             comboBoxCourses.getItems().clear();
             textAreaCourses.clear();
             labelECTSProgram.setText("0");
             if (selectedProgram != null) {
+
+                // Kurser vises som er relateret til det valgte program
                 comboBoxCourses.getItems().addAll(model.baseCourse(selectedProgram));
 
+                // Projekter vises som er relateret til det valgte program
                 List<String> project = model.baseProject(selectedProgram);
                 if (project != null) {
                     comboBoxCourses.getItems().addAll(project);
@@ -83,18 +97,25 @@ public class ApplicationTest extends Application {
             }
             updateTotalECTS();
         });
+
+        // Brugerne vælger et kursus som kan vælges ud fra programkurser
         comboBoxCourses.setOnAction(event->{
             String selectedCourse = comboBoxCourses.getValue();
+            //Tjekker om kurset allerede er blevet tilføjet
             if (selectedCourse != null && !textAreaCourses.getText().contains(selectedCourse)){
+            // Tilføjer kurset til tektsområdet
             textAreaCourses.appendText(selectedCourse + "\n");
+            // Opdaterer ECTS-point
             int current = Integer.parseInt(labelECTSProgram.getText());
             labelECTSProgram.setText(String.valueOf(current + model.courseWeight(selectedCourse)));
             updateTotalECTS();
+            // Gemmer de valgte kursuser
             model.saveCourse("Safi", selectedCourse);
             }
         });
     }
 
+    // Laver første fagemne sektion
     private void setupSubject1Selection() {
     comboBoxSubject1.getItems().addAll(model.subjectModule());
     comboBoxSubject1.setOnAction(event -> {
@@ -118,6 +139,7 @@ public class ApplicationTest extends Application {
         }
     });
 }
+// Opsætter andet fagemne sektion
 private void setupSubject2Selection() {
     comboBoxSubject2.getItems().addAll(model.subjectModule());
 
@@ -143,6 +165,7 @@ private void setupSubject2Selection() {
 
     });
 }
+// Opsætter valgfag sektionen
 private void setupElectivesSelection() {
     comboBoxElectives.getItems().addAll("Functional Programming","Scientific Computing");
     comboBoxElectives.setOnAction(event -> {
@@ -157,6 +180,7 @@ private void setupElectivesSelection() {
     });
   }
 
+  // Lægger alle elementer i Gridplane Layoutet, og placering kan styres
   private void setupLayout(GridPane root) {
     root.add(label1,0,0);
     root.add(comboBoxProgram, 0, 1);
@@ -182,6 +206,7 @@ private void setupElectivesSelection() {
     root.add(labelECTSElectives,3,4);
     root.add(labelECTSTotal,3,5);
     }
+    // Opdatere den samelde ECTS-point og tilføje det i label
     private void updateTotalECTS() {
         int total = Integer.parseInt(labelECTSProgram.getText())+
                 Integer.parseInt(labelECTSSubject1.getText())+
@@ -189,6 +214,7 @@ private void setupElectivesSelection() {
                 Integer.parseInt(labelECTSElectives.getText());
         labelECTSTotal.setText("Total ECTS: " + total);
     }
+    // Main-metoden som starter applikationen
     public static void main(String[] args) {
         launch(args);
     }
